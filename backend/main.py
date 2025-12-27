5aceb7e1a8466ec63aa189472a06eafa6b0ab5a5
abdoulousseini2028-droid <abdoulousseini2028@u.northwestern.edu>
2025-12-27 17:35:42 -0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -63,4 +63,5 @@ async def websocket_endpoint(websocket: WebSocket):
 
 # THIS PART KEEPS THE SERVER RUNNING
 if __name__ == "__main__":
+    import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
